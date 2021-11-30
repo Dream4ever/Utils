c750c2694b98d1000d1258291c5b9d58864d8400
Dream4ever <cnhwvista@gmail.com>
2021-11-30 17:01:46 +0800

    feat: 添加清除终端当前行文本的工具函数

--- utils.js
+++ utils.js
@@ -1,7 +1,15 @@
+import process from 'process'
+
 const log = (...args) => console.log(...args)
 const throwError = (msg) => { throw new Error(msg) }
+
+const clearLine = () => {
+  process.stdout.moveCursor(0, -1)
+  process.stdout.clearLine(1)
+}
 
 export {
   log,
   throwError,
+  clearLine,
 }
